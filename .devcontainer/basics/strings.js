7c52c84f7bac0a7318596a19bde2c886669cf62e
parthsharma-07 <parth.damodarm@gmail.com>
2025-12-09 07:15:39 +0000

    changes

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,15 @@
+let str1="helo"
+let str2="parth"
+str3=str1+str2;
+// console.log(str3)
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(null>0); //false
+console.log(null>0)//false
+//
+console.log(null>=0)//true
+
+  
+
